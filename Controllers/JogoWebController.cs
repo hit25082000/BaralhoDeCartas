ca1a3dd9ed1c1018866996e0eb9ee1cef157768f
Luiz Eduardo Domingues <ldomingues@pge.ms.gov.br>
2025-03-19 10:56:40 -0400

    CSS e View de jogos top

--- Controllers/JogoWebController.cs
+++ Controllers/JogoWebController.cs
@@ -1,21 +1,68 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using BaralhoDeCartas.Services.Interfaces;
+using BaralhoDeCartas.Models.Interfaces;
 
 namespace BaralhoDeCartas.Controllers   
 {    
     public class JogoWebController : Controller
     {
         private readonly IJogoService _jogoService;
+        private readonly IBlackjackService _blackjackService;
 
-        public JogoWebController(IJogoService jogoService)
+        public JogoWebController(IJogoService jogoService, IBlackjackService blackjackService)
         {
             _jogoService = jogoService;
+            _blackjackService = blackjackService;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string jogo)
         {
-            var jogo = await _jogoService.IniciarNovoJogo();
-            return View(jogo); 
+            if (string.IsNullOrEmpty(jogo))
+            {
+                return RedirectToAction("Index", "Jogos");
+            }
+
+            switch (jogo.ToLower())
+            {
+                case "maiorcarta":
+                    var jogoMaiorCarta = await _jogoService.IniciarNovoJogo();
+                    return View("MaiorCarta", jogoMaiorCarta);
+                
+                case "blackjack":
+                    var jogoBlackjack = await _blackjackService.IniciarJogo();
+                    return View("Blackjack", jogoBlackjack);
+                
+                default:
+                    return RedirectToAction("Index", "Jogos");
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DistribuirCartas(string baralhoId, int numeroJogadores)
+        {
+            var jogadores = await _jogoService.DistribuirCartas(baralhoId, numeroJogadores);
+            return Json(jogadores);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> IniciarRodada(string baralhoId, int numeroJogadores)
+        {
+            var jogadores = await _blackjackService.IniciarRodada(baralhoId, numeroJogadores);
+            return Json(jogadores);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ComprarCarta(string baralhoId, IJogadorDeBlackjack jogadorId)
+        {
+            var jogador = await _blackjackService.ComprarCarta(baralhoId, jogadorId);
+            return Json(jogador);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> FinalizarJogo(string baralhoId)
+        {
+            await _jogoService.FinalizarJogo(baralhoId);
+            return RedirectToAction("Index", "Jogos");
         }
     }
 }
---
+++ Pages/Jogos/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BaralhoDeCartas.Pages.Jogos
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+} 
